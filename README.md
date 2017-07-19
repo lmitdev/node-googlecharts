21d3a139ab16e17d39540c0ef7e910df2752f387
Renato Pestana <renatompestana@gmail.com>
2017-07-19 09:06:31 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 
 [![Build Status][travis-image]][travis-url]
 
+## Description
+This project is an adaptation of the original [node-googlecharts by zallek](https://travis-ci.org/zallek/node-googlecharts), all credits go to zallek.
+
+The original project uses the old GoogleCharts API so I decided to update it, right now it uses the latest GoogleCharts API and also the last JSDOM version, also I added some new features.
+
+**node-googlecharts** allows you to have a server-side graph generation tool that can than deliver the result to the client via something like an API.
+
 ## Requirements
 - Node.js > 4.0
-- ICU4C
+- ICU4
+- node-canvas
 
 
 ## ICU4C dependency
@@ -12,18 +20,43 @@ Google Chart needs full internationalization support whereas it's not built in b
 - Use [full-icu](https://www.npmjs.com/package/full-icu) npm package which requires running node with a specific environment  variable.
 - Or, build Node.js with an embedded icu. [more info](https://github.com/nodejs/node/wiki/Intl#building-node-with-an-embedded-icu)
 
+### How to use
+**node-googlecharts** allows you to extrat a chart image both in SVG or PNG(Base64) format. To do so you simply have to call the `render()` function with the `<ChartWrapperOptions>`, `<ExtractFormat>` and `<ForceUncutLine>` as parameters.
+
+| Argument | Type | Description |
+| ------ | ------ | ------ |
+| `ChartWrapperOptions` | JSON | Uses default [GoogleCharts Options](https://developers.google.com/chart/interactive/docs/) |
+| `ExtractFormat` | String | Can either be `svg` or `png` |
+| `ForceUncutLine` | Boolean | Forces legends to be single line without being cut off |
 
 ### Run binary
 ```SH
-node bin/node-googlecharts <ChartWrapperOptions>
+$ node bin/node-googlecharts '<ChartWrapperOptions>'
+or
+$ npm run bin '<ChartWrapperOptions>'
 ```
 `ChartWrapperOptions` is the serialized JSON options to give to `ChartWrapper`.
+```sh
+{
+   "chartType": "ColumnChart",
+   "dataTable": [
+      ["", "Germany", "USA", "Brazil", "Canada", "France", "RU"],
+      ["", 700, 300, 400, 500, 600, 800]
+   ],
+   "options": {
+      "title": "Countries"
+   }
+}
+```
 
+### Install as node module
+If you wish to use this project as a node_module to use in your API or other projects, simple type the following command in your command line:
+```sh
+$ npm install 3nvy/node-googlecharts --save
+```
+Then you just need to require the project name and its done.
 
 ## FAQ
-
-#### Who uses it?
-[lambda-googlechart](https://github.com/zallek/lambda-googlecharts) uses it to generate charts images on AWS Lambda.
 
 #### Why jsdom?
 Google Charts needs a browser-like environment to run in. Jsdom is a light DOM implementation, much faster than PhantomJS.
@@ -31,8 +64,8 @@ Google Charts needs a browser-like environment to run in. Jsdom is a light DOM i
 #### Why Node.js > 4.0?
 This is a requirement of jsdom itself.
 
-#### Why is Google Chart library loaded from local files?
-The way Google Chart usually loads its dependencies is incompatible with jsdom [more info](https://github.com/tmpvar/jsdom/issues/1463)
+#### Why node-canvas
+We need a virtual canvas to be able to calculate the currect size of the chart legends, allowing for text wrapping
 
 #### Why does it need ICU4C?
 Google Chart needs full internationalization support whereas it's not built in by default on nodejs.
--- src/index.js
+++ src/index.js
@@ -1,131 +1,174 @@
-const jsdom = require('jsdom');
 const isPlainObject = require('lodash.isplainobject');
+const Canvas = require('canvas-prebuilt');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+/**
+ * Hotfix to calculate text width/height so Google Charts
+ * can separate labels into different lines
+ * 
+ * @param {window} window 
+ * @param {element} element 
+ * @returns {JSON}
+ */
+function measureTextSize(window, element) {
+    const canvas = new Canvas(0, 0);
+    const ctx = canvas.getContext("2d");
+    const fontSize = window.getComputedStyle(element).fontSize || 13;
+    const fontFamily = window.getComputedStyle(element).fontFamily || 'arial';
 
+    ctx.font = `${fontSize}px ${fontFamily}`;  // This can be set programmaticly from the element's font-style if desired
+    return { width: ctx.measureText(element.innerHTML).width, height: fontSize + 1 };
+}
 
 /**
 * offsetWidth && offsetHeight weren't implemented. The implementation is taken from
 * https://github.com/tmpvar/jsdom/issues/135#issuecomment-68191941
 * clientWidth && clientHeight had the same issue (the implementation is wrong but it's good enough for Google Chart)
 */
-function applyJsdomWorkaround(window) {
-  Object.defineProperties(window.HTMLElement.prototype, {
-    clientHeight: {
-      configurable: true,
-      get: function () {
-        return parseFloat(window.getComputedStyle(this).height) || 0;
-      }
-    },
-    offsetHeight: {
-      configurable: true,
-      get: function () {
-        return parseFloat(window.getComputedStyle(this).height) || 0;
-      }
-    },
-    clientWidth: {
-      configurable: true,
-      get: function () {
-        return parseFloat(window.getComputedStyle(this).width) || 0;
-      }
-    },
-    offsetWidth: {
-      configurable: true,
-      get: function () {
-        return parseFloat(window.getComputedStyle(this).width) || 0;
-      }
-    },
-  });
-}
-
-function createGoogleChartWindow(args) {
-  return new Promise((resolve, reject) => {
-    const baseHtml = (
-      `<html><head>
-        <script src="file:///${__dirname}/jsapi.js"></script>
-        <script src="file:///${__dirname}/google-charts.js"></script>
-      </head><body></body></html>`
-    );
-    const window = jsdom.jsdom(baseHtml, {
-      features: {
-        FetchExternalResources: ['script'],
-        ProcessExternalResources: ['script'],
-      },
-    }).defaultView;
-    window.addEventListener('load', event => {
-      applyJsdomWorkaround(window);
-      resolve({
-        window,
-        chartOptions: args.chartOptions,
-        format: args.format,
-      });
-    }, 2000);
-    // virtualConsole: jsdom.createVirtualConsole().sendTo(console),
-  }).catch(error =>
-    Promise.reject(new Error('[ChartInitError] ' + error.message))
-  );
-}
-
-function renderChart(args) {
-  return new Promise((resolve, reject) => {
-    const window = args.window;
-    const chartOptions = args.chartOptions;
-
-    // Create container
-    const container = window.document.createElement('div');
-    container.id = chartOptions.containerId;
-    container.setAttribute(
-      'style',
-      `width:${chartOptions.options.width}px;height:${chartOptions.options.height}px;`
-    );
-    window.document.body.appendChild(container);
-
-    // Render chart
-    const wrapper = new window.google.visualization.ChartWrapper(chartOptions);
-    window.google.visualization.events.addListener(wrapper, 'ready', () => {
-      resolve(args);
+function applyJsdomWorkaround(window, forceUncutLine) {
+    Object.defineProperties(window.HTMLElement.prototype, {
+        clientHeight: {
+            configurable: true,
+            get: function () {
+                return (forceUncutLine) ? 0 : measureTextSize(window, this).height || 0;
+            }
+        },
+        offsetHeight: {
+            configurable: true,
+            get: function () {
+                return parseFloat(window.getComputedStyle(this).height) || 0;
+            }
+        },
+        clientWidth: {
+            configurable: true,
+            get: function () {
+                return (forceUncutLine) ? 0 : measureTextSize(window, this).width || 0;
+            }
+        },
+        offsetWidth: {
+            configurable: true,
+            get: function () {
+                return parseFloat(window.getComputedStyle(this).width) || 0;
+            }
+        },
     });
-    window.google.visualization.events.addListener(wrapper, 'error', error => {
-      reject(error);
-    });
-    wrapper.draw();
-  }).catch(error =>
-    Promise.reject(new Error('[RenderingError] ' + error.message))
-  );
 }
-
-function extractSVG(args) {
-  const window = args.window;
-  const chartOptions = args.chartOptions;
-
-  return window.document
-    .querySelector('#' + chartOptions.containerId + ' svg').outerHTML;
-}
-
 
 /**
- * Render a Google Chart to a png image
- * @param  {chartOptions} Object Google ChartWrapper options
- * @param  {format} String 'svg'
- * @return {Promise}
+ * Pre-Generates GoogleChart with given options 
+ * 
+ * @param {args} Object {chartOptions, format} 
+ * @returns {Promise}
  */
-function render(chartOptions, format) {
-  format = format || 'svg';
+function generateChart(args) {
+    return new Promise((resolve, reject) => {
 
-  if (format !== 'svg') {
-    return Promise.reject(new Error('[InputError] unsupported format'));
-  }
-  if (!isPlainObject(chartOptions)) {
-    return Promise.reject(new Error('[InputError] chartOptions should be an object containing Google ChartWrapper options'));
-  }
+        //Generate Virtual JS DOM
+        const { window } = new JSDOM(
+            `<html><head>
+             <script src="https://www.gstatic.com/charts/loader.js"></script>
+             </head><body></body></html>`,
+            { resources: "usable", runScripts: "dangerously" }
+        );
 
-  // Default chartOptions
-  chartOptions.containerId = 'vis_div';
-  chartOptions.options = chartOptions.options || {};
-  chartOptions.options.width = chartOptions.options.width || 600;
-  chartOptions.options.height = chartOptions.options.height || 400;
+        //JSDOM document load event
+        window.addEventListener('load', event => {
+            //Load GoogleCharts lib
+            window.google.charts.load("current", { packages: ['corechart'] });
+            window.google.charts.setOnLoadCallback(() => {
+                applyJsdomWorkaround(window, args.forceUncutLine)
+                resolve({
+                    window,
+                    chartOptions: args.chartOptions,
+                    format: args.format
+                })
+            });
+        }, false);
+    })
+        .catch(error =>
+            Promise.reject(new Error('[ChartInitError] ' + error.message))
+        );
+}
 
-  return createGoogleChartWindow({ chartOptions, format })
-    .then(renderChart)
-    .then(extractSVG)
+/**
+ * Render GoogleChart at JSDOM
+ * 
+ * @param {args} Object {window, chartOptions, format}
+ * @returns {Promise}
+ */
+function renderChart(args) {
+    return new Promise((resolve, reject) => {
+        const window = args.window,
+            chartOptions = args.chartOptions,
+            format = args.format,
+            container = window.document.createElement('div');
+
+        container.id = chartOptions.containerId;
+        container.setAttribute(
+            'style',
+            `width:${chartOptions.options.width}px;height:${chartOptions.options.height}px;`
+        );
+        window.document.body.appendChild(container);
+
+        // Render chart
+        const wrapper = new window.google.visualization.ChartWrapper(chartOptions);
+        window.google.visualization.events.addListener(wrapper, 'ready', () => {
+            if (format === 'png')
+                args.b64 = wrapper.getChart().getImageURI();
+
+            resolve(args);
+        });
+        window.google.visualization.events.addListener(wrapper, 'error', error => {
+            reject(error);
+        });
+        wrapper.draw();
+    })
+        .catch(error =>
+            Promise.reject(new Error('[RenderingError] ' + error.message))
+        );
+}
+
+/**
+ * Extract either svg or png base64 from JSDOM
+ * 
+ * @param {args} Object {window, chartOptions, format}
+ * @returns {Promise}
+ */
+function extractImage(args) {
+    const window = args.window,
+        chartOptions = args.chartOptions,
+        format = args.format;
+
+    return (format === 'png') ? args.b64 : window.document
+        .querySelector('#' + chartOptions.containerId + ' svg').outerHTML;
+}
+
+/**
+ * Render a Google Chart to a svg/png image
+ * 
+ * @param {chartOptions} Object Google ChartWrapper options
+ * @param {format} String 'svg' || 'png(b64)' 
+ * @returns {Promise}
+ */
+function render(chartOptions, format, forceUncutLine) {
+    format = format || 'svg';
+
+    if (format !== 'svg' && format !== 'png') {
+        return Promise.reject(new Error('[InputError] unsupported format'));
+    }
+    if (!isPlainObject(chartOptions)) {
+        return Promise.reject(new Error('[InputError] chartOptions should be an object containing Google ChartWrapper options'));
+    }
+
+    // Default chartOptions
+    chartOptions.containerId = 'vis_div';
+    chartOptions.options = chartOptions.options || {};
+    chartOptions.options.width = chartOptions.options.width || 600;
+    chartOptions.options.height = chartOptions.options.height || 400;
+
+    return generateChart({ chartOptions, format, forceUncutLine })
+        .then(renderChart)
+        .then(extractImage)
 }
 
 module.exports = render;
